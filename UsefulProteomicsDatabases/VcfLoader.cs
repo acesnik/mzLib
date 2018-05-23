6a06805af8d56f58cfae91bb5842a3c45752045c
acesnik <acesnik@chem.wisc.edu>
2018-05-23 14:52:58 -0500

    started vcf stuff

---
+++ UsefulProteomicsDatabases/VcfLoader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Bio.VCF;
+using Proteogenomics;
+
+namespace UsefulProteomicsDatabases
+{
+    public class VcfLoader
+    {
+        public VcfLoader(string vcfPath)
+        {
+
+        }
+    }
+}
